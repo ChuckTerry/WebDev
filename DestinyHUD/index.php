24e8ff590bdb60953422d144b1ab6c90758054d0
Chuck Terry <ct.jcpm@gmail.com>
2017-10-28 21:40:05 -0400

    Create index.php
    Added the home page's source code.  Not much here, I just need it in my workspace.

---
+++ DestinyHUD/index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+  <link rel="shortcut icon"href="favicon.ico">
+  <title>Destiny HUD</title>
+  </head>
+  <body>
+    <p>This app is currently in development, no code runs from this page, therefor authorizing the app will do nothing unless you inject the code.</p><br/>
+    <a href="https://www.bungie.net/en/OAuth/Authorize?client_id=21910&response_type=code&state=DebugState">Authorize App</a>
+  </body>
+</html>
