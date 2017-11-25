2a50159b8db8153b43290473fb3c0012796c1143
Chuck Terry <ct.jcpm@gmail.com>
2017-11-25 02:44:12 -0500

    Multiple Updates
    Update copyright; Change global variable check; Migrated functions out of global namespace; Added proper initialization check and removed console warning; Changed string wrappers to back ticks;

--- SiteTreeUtility/Source.js
+++ SiteTreeUtility/Source.js
@@ -1,5 +1,5 @@
 // SiteTreeUtility - Shows the DOM tree layout of the current webpage.
-//     Copyright (C) 2016  Charles Vance Terry
+//     Copyright (C) 2016 - 2017 Charles Vance Terry
 //     Email: ct.jcpm@gmail.com
 // 
 //     This program is free software: you can redistribute it and/or modify
@@ -17,54 +17,52 @@
 // End License
 
 /** Global Object */
-var jcpm; if (jcpm === undefined) { jcpm = {}; }
+if (typeof jcpm === `undefined`) jcpm = {};
 
 /**
  Initializer
 */
-function walkTreeStart(node) {
-  jcpm.walkTreeOutput = "HTML Tree Layout:\r\n"; // (Re)set Tree.
-  if (node == null) {
-    walkTree(document.body, 0);                  // Default to <body>
-  } else {
-    walkTree(node, 0);                           // Start
-  }
-  jcpm.walkTreeOutput += "End of HTML Tree";     // Add Footer to Tree
-  displayOutput();                               // Show Output in PopUp Window
+jcpm.walkTreeStart = function(node) {
+  jcpm.init = true;                               // Set Init Flag
+  jcpm.walkTreeOutput = `HTML Tree Layout:\r\n`;  // Reset Tree/Add Header
+  node = node || document.body;
+  jcpm.walkTree(node, 0);                         // Start
+  jcpm.walkTreeOutput += `End of HTML Tree`;      // Add Footer to Tree
+  jcpm.displayOutput();                           // Show Output in PopUp Window
+  jcpm.init = false;                              // Clear Init Flag
 }
-
 
 /**
  Tree Walker
 */
-function walkTree(node, depth) {
-  if ((node || depth) == null) {  // Ensure node is Specified
-    console.log("Use 'walkTreeStart();' to ensure proper initialization.");
+jcpm.walkTree = function(node, depth) {
+  if (!jcpm.init) {  // Ensure Proper Initialization
+    jcpm.walkTreeStart();
     return;
   }
 
   /** Output Various Node Details */
-  if (typeof node.tagName !== "undefined") {
-    for (i = 0; i < depth; i++) { 
-      jcpm.walkTreeOutput += "  :";                                  // Indention & Guides
+  if (typeof node.tagName !== `undefined`) {
+    for (let i = 0; i < depth; i++) { 
+      jcpm.walkTreeOutput += `  :`;                                  // Indention & Guides
     }
-    jcpm.walkTreeOutput += ("<" + node.tagName.toLowerCase() + ">"); // Element Type
-    jcpm.walkTreeOutput += "    ";                                   // Element-Class Delimiter
+    jcpm.walkTreeOutput += (`<` + node.tagName.toLowerCase() + `>`); // Element Type
+    jcpm.walkTreeOutput += `    `;                                   // Element-Class Delimiter
     if (node.classList.length !== 0) {
-      for (i = 0; i < node.classList.length; i++) {
-        jcpm.walkTreeOutput += ("." + node.classList[i]);            // Element Class Name(s)
+      for (let i = 0; i < node.classList.length; i++) {
+        jcpm.walkTreeOutput += (`.` + node.classList[i]);            // Element Class Name(s)
       }
     }
-    jcpm.walkTreeOutput += "    ";                                   // Element-ID Delimiter
-    if (node.id !== "") {
-      jcpm.walkTreeOutput += ("#" + node.id);                        // Element ID
+    jcpm.walkTreeOutput += `    `;                                   // Element-ID Delimiter
+    if (node.id !== ``) {
+      jcpm.walkTreeOutput += (`#` + node.id);                        // Element ID
     }
-    jcpm.walkTreeOutput += "\r\n";                                   // New Line
+    jcpm.walkTreeOutput += `\r\n`;                                   // New Line
   }
   
-  /** Recursion to Work Through the Tree */
-  for (var i = 0; i < node.childNodes.length; i++) {
-    walkTree(node.childNodes[i], (depth + 1));
+  // Recursion to Work Through the Tree
+  for (let i = 0; i < node.childNodes.length; i++) {
+    jcpm.walkTree(node.childNodes[i], (depth + 1));
   }
 }
 
@@ -72,12 +70,12 @@ function walkTree(node, depth) {
 /**
   PopUp Window Creator
   */
-function displayOutput() {
-  var popUp = window.open("", "", "width=300,height=400");
-  var node = document.createElement("TEXTAREA");
-  var outputText = document.createTextNode(jcpm.walkTreeOutput);
+jcpm.displayOutput = function() {
+  let popUp = window.open(``, ``, `width=1200,height=600`);
+  let node = document.createElement(`TEXTAREA`);
+  let outputText = document.createTextNode(jcpm.walkTreeOutput);
   node.appendChild(outputText);
-  node.setAttribute("warp", "soft");
-  node.setAttribute("style", "margin: 0px;border: 0px;width:100%;height:100%;");
+  node.setAttribute(`warp`, `soft`);
+  node.setAttribute(`style`, `margin: 0px;border: 0px;width:100%;height:100%;`);
   popUp.document.body.appendChild(node);
 }
