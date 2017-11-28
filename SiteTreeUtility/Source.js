33a5d85d637f1a68a61fa06caa5607b7e74d04ce
Chuck Terry <ct.jcpm@gmail.com>
2017-11-27 20:08:20 -0500

    Cleanup
    Removed trailing spaces, added missing semicolons; added missing var statement on global declaration.

--- SiteTreeUtility/Source.js
+++ SiteTreeUtility/Source.js
@@ -1,23 +1,23 @@
 // SiteTreeUtility - Shows the DOM tree layout of the current webpage.
 //     Copyright (C) 2016 - 2017 Charles Vance Terry
 //     Email: ct.jcpm@gmail.com
-// 
+//
 //     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
 //     the Free Software Foundation, either version 3 of the License, or
 //     (at your option) any later version.
-// 
+//
 //     This program is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
-// 
+//
 //     You should have received a copy of the GNU General Public License
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // End License
 
 /** Global Object */
-if (typeof jcpm === `undefined`) jcpm = {};
+if (typeof jcpm === `undefined`) var jcpm = {};
 
 /**
  Initializer
@@ -30,7 +30,7 @@ jcpm.walkTreeStart = function(node) {
   jcpm.walkTreeOutput += `End of HTML Tree`;      // Add Footer to Tree
   jcpm.displayOutput();                           // Show Output in PopUp Window
   jcpm.init = false;                              // Clear Init Flag
-}
+};
 
 /**
  Tree Walker
@@ -43,7 +43,7 @@ jcpm.walkTree = function(node, depth) {
 
   /** Output Various Node Details */
   if (typeof node.tagName !== `undefined`) {
-    for (let i = 0; i < depth; i++) { 
+    for (let i = 0; i < depth; i++) {
       jcpm.walkTreeOutput += `  :`;                                  // Indention & Guides
     }
     jcpm.walkTreeOutput += (`<` + node.tagName.toLowerCase() + `>`); // Element Type
@@ -59,12 +59,12 @@ jcpm.walkTree = function(node, depth) {
     }
     jcpm.walkTreeOutput += `\r\n`;                                   // New Line
   }
-  
+
   // Recursion to Work Through the Tree
   for (let i = 0; i < node.childNodes.length; i++) {
     jcpm.walkTree(node.childNodes[i], (depth + 1));
   }
-}
+};
 
 
 /**
@@ -78,4 +78,4 @@ jcpm.displayOutput = function() {
   node.setAttribute(`warp`, `soft`);
   node.setAttribute(`style`, `margin: 0px;border: 0px;width:100%;height:100%;`);
   popUp.document.body.appendChild(node);
-}
+};
